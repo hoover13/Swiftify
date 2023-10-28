1031f92451522d294b16d721976a4cfa7a39e6d0
Min Thu Khine <helloworldjustin9@gmail.com>
2023-10-28 15:37:54 -0400

    - add assets and data - parse json file and style data in ContentModel successfully

--- Swiftify/Views/HomeView.swift
+++ Swiftify/Views/HomeView.swift
@@ -25,5 +25,6 @@ struct HomeView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         HomeView()
+            .environmentObject(ContentModel())
     }
 }
