4424e8f4dbc38dfc0c37b8617f1d3f4331674f15
Min Thu Khine <helloworldjustin9@gmail.com>
2023-10-28 00:33:19 -0400

    Initial Commit

---
+++ Swiftify/SwiftifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftifyApp.swift
+//  Swiftify
+//
+//  Created by Hoover on 10/28/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
